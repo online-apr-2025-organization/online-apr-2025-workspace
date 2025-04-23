4aeb83af5b52138bbb961ca74cc33307d73e56d2
bpoongs <bpoongs@gmail.com>
2025-04-23 15:34:18 +0530

    Initial commit

---
+++ DemoProject/src/LoopDemo.java
@@ -0,0 +1,21 @@
+
+public class LoopDemo {
+
+	public static void main(String[] args) {
+		
+		int mark = 50;
+		String msg = "hello";
+		System.out.println("Good Evening!!");
+		System.out.println("Good Evening!!");
+		System.out.println("Good Evening!!");
+		System.out.println("Good Evening!!");
+		System.out.println("Good Evening!!");
+		
+		System.out.println("------------------------");
+		// intial value, condition, exp
+		for(int i=1;i<=5;i++) {
+			System.out.println("Good Evening!!");
+		}
+	}
+
+}
